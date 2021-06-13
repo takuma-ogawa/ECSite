988981fcc2e986fea8341fe8149df79fcd451300
takuma-ogawa <takumaogawa126@icloud.com>
2021-06-13 13:53:38 +0900

    axiosのget完了

--- ClothesShopping/front/urls.py
+++ ClothesShopping/front/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('detail/<int:pk>', views.ProductDetail.as_view(), name='detail'),
     path('vue', views.ShowProductList.as_view(), name='vue'),
+    path('product_list', views.get_product_list, name='product_list')
 ]
